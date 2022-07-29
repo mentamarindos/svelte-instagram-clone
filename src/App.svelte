a44d2a35f77fe1f6de0366b699c2e870f3b8df5a
mentamarindos <mentamarindos@github.com>
2022-07-28 23:48:16 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+
+ <p class="text-center">Svelte</p>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+
+</main>
