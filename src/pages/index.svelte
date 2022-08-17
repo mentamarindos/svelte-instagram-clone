40c96348fc8d7b27655808b8268d4836db2bfa7d
mentamarindos <mentamarindos@github.com>
2022-08-17 07:02:28 -0600

    Add signUp form

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -7,11 +7,12 @@ import { url } from "@roxi/routify";
 
 <!-- html  --> 
 <main class="grid mt-10 pt-5 place-items-center"> 
-    <h1 class="m-3">root page</h1>
-    <MockupPhone />
+
+    <h1 class="m-10 text-3xl">Welcome</h1>
 
     <nav class="card p-2 m-4">
         <a href={$url('/login')} >Login</a>
         <a href={$url('/signup')} >Signup</a>
     </nav>
+
 </main>
