7d3afce65e90d45587c5f56d1f3946476c15dffd
mentamarindos <mentamarindos@github.com>
2022-08-17 05:21:09 -0600

    Add mockupPhone a la pagina principal

--- .routify/config.js
+++ .routify/config.js
@@ -4,7 +4,7 @@ module.exports = {
   "routifyDir": ".routify",
   "ignore": "",
   "dynamicImports": true,
-  "singleBuild": true,
+  "singleBuild": false,
   "noHashScroll": false,
   "distDir": "dist",
   "hashScroll": true,
@@ -14,5 +14,5 @@ module.exports = {
     "md",
     "svx"
   ],
-  "started": "2022-08-17T10:45:33.380Z"
+  "started": "2022-08-17T11:16:09.279Z"
 }
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,3 +1,9 @@
-<main>
-    <h1>Pagina principal</h1>
+<script>
+    import MockupPhone from "@/components/mockupPhone.svelte";
+</script>
+
+<!-- html  -->
+<main class="grid mt-10 place-items-center">
+    <h1 class="">Pagina principal</h1>
+    <MockupPhone />
 </main>
