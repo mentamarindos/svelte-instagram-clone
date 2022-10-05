80a722c7f4f4c9d9b1f61a30197f7c38b57e28b3
mentamarindos <mentamarindos@github.com>
2022-10-04 22:42:40 -0600

    Add supabase service

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export { supabase } 
